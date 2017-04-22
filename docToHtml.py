99bb8e28b09ee55d9b0e94c4e9b7dfeec5840f2d
Archana Chandrasekharan <kcarchana77@gmail.com>
2017-04-22 20:49:26 +0530

    Update docToHtml.py

--- docToHtml.py
+++ docToHtml.py
@@ -4,13 +4,13 @@ from tidylib import tidy_document
 
 output = pypandoc.convert(sys.argv[1], 'html', extra_args=['--extract-media=./'])
 
-// tidy_document returns both output and errors and we just errors to the terminal
+# tidy_document returns both output and errors and we just errors to the terminal
 output, errors = tidy_document(output, options={
         'numeric-entities': 1,
         'wrap': 80,
 })
 print(errors)
-// Replace smart quotes with normal quote
+# Replace smart quotes with normal quote
 output = output.replace(u"\u2018", '&lsquo;').replace(u"\u2019", '&rsquo;')
 output = output.replace(u"\u201c", "&ldquo;").replace(u"\u201d", "&rdquo;")
 
